5a1d3c8c0fe5cf61b8c0cc901742a0db8998d4b1
Tim Pietz <t.pietz@evasys.de>
2022-10-28 14:18:15 +0200

    Add a rough sketch implementation of the operator interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,51 @@
+export type Size = [width: number, height: number];
+
+export interface WordcloudConfig {
+    size: Size,
+    fontFamily: string,
+    fontWeight: number | string,
+    fontSize: number,
+}
+
+export class Wordcloud {
+    constructor(private readonly config: WordcloudConfig) {
+    }
+
+    size = this.configAccessor('size');
+    fontSize = this.configAccessor('fontSize');
+    fontWeight = this.configAccessor('fontWeight');
+    fontFamily = this.configAccessor('fontFamily');
+
+    start() {
+        return this;
+    }
+
+    stop() {
+        return this;
+    }
+
+    configAccessor<Property extends keyof WordcloudConfig>(property: Property) {
+        const cloud = this;
+
+        function accessor(): WordcloudConfig[Property];
+        function accessor<Value extends WordcloudConfig[Property]>(value: Value): Wordcloud;
+        function accessor(...args: [] | [WordcloudConfig[Property]]) {
+            if (args.length === 0) {
+                return cloud.config[property];
+            } else {
+                return new Wordcloud({...cloud.config, [property]: args[0]})
+            }
+        }
+
+        return accessor;
+    }
+}
+
 export const wordcloud = () => {
-    return 'Hello World 123';
+    return new Wordcloud({
+        size: [512, 512],
+        fontFamily: 'sans-serif',
+        fontWeight: 400,
+        fontSize: 12,
+    })
 }
