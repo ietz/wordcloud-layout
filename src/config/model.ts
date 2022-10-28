543f6c4f6f72183996e48b65eb807fe60b31dd84
Tim Pietz <t.pietz@evasys.de>
2022-10-28 14:22:30 +0200

    Reorganize code

---
+++ src/config/model.ts
@@ -0,0 +1,8 @@
+export type Size = [width: number, height: number];
+
+export interface WordcloudConfig {
+  size: Size,
+  fontFamily: string,
+  fontWeight: number | string,
+  fontSize: number,
+}
