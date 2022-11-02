2e75a09dc3194075832ba5d369a7bae6cefc4bdf
Tim Pietz <t.pietz@evasys.de>
2022-11-02 16:43:41 +0100

    Remove unused color map

--- src/layout/sprites/compute.ts
+++ src/layout/sprites/compute.ts
@@ -9,8 +9,6 @@ export const computeSprites = (config: WordcloudConfig) => {
   document.getElementById('app')!.appendChild(canvas);
   const ctx = canvas.getContext('2d')!;
 
-  const colors = ["#4e79a7", "#f28e2c", "#e15759", "#76b7b2", "#59a14f", "#edc949", "#af7aa1", "#ff9da7", "#9c755f", "#bab0ab"];
-
   const measurements = config.data.map(datum => measureText(ctx, config, datum));
   const {placements, ...canvasSize} = boxPack(measurements.map(measurement => [measurement.boxWidth, measurement.boxHeight]));
 
